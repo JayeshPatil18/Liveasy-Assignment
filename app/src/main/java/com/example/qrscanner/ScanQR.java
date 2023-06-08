788ef4110cc296782100aa9573c402fd89a0063a
Jayesh Patil <jp7470484@gmail.com>
2023-06-08 00:25:44 -0700

    second commit

--- app/src/main/java/com/example/qrscanner/ScanQR.java
+++ app/src/main/java/com/example/qrscanner/ScanQR.java
@@ -1,14 +1,56 @@
 package com.example.qrscanner;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class ScanQR extends AppCompatActivity {
+
+    Button scanbtn;
+    TextView scannedValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
+
+        scanbtn = findViewById(R.id.scanCode);
+        scannedValue = findViewById(R.id.valuetext);
+
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanCode();
+            }
+        });
+
+        scanCode();
     }
+
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            scannedValue.setText(result.getContents());
+        }
+    });
 }
